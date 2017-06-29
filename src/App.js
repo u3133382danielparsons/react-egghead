29c0d0fd4cd295e160ce4a89df63dd42c020a897
Daniel Parsons <danielparsons3133382@gmail.com>
2017-06-30 00:41:46 +1000

    Error handling on props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react'
 
 class App extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      txt: 'this is the state txt'
-    }
-  }
-  update(e){
-    this.setState({txt: e.target.value})
-  }
+
   render(){
     return(
-      <div>
-        <h1>{this.state.txt}</h1>
-        <Widget update={this.update.bind(this)} />
-      </div>
+      <Title text="Thext" />
     )
   }
 }
 
-const Widget = (props =>
-   <input type="text" onChange={props.update} />)
+const Title = (props) => <h1>Title: {props.text}</h1>
+
+Title.propTypes = {
+  text(props, propName, component){
+    if(!(propName in props)){
+      return new Error(`missing ${propName}`)
+    }
+    if(props[propName].length < 6){
+      return new Error(`${propName} was too short`)
+    }
+  }
+}
+
 export default App
