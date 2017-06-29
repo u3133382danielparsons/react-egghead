d07b916d87cb57a70521b06b18807fe6edf5fcec
Daniel Parsons <danielparsons3133382@gmail.com>
2017-06-30 00:54:27 +1000

    Events occuring in textarea displayed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react'
 
 class App extends React.Component {
-
+  constructor(){
+    super()
+    this.state = {currentEvent: '---'}
+    this.update = this.update.bind(this)
+  }
+  update(e){
+    this.setState({currentEvent: e.type})
+  }
   render(){
     return(
-      <Title text="Thext" />
+      <div>
+        <textarea
+          onKeyPress={this.update}
+          onCopy={this.update}
+          onCut={this.update}
+          onPaste={this.update}
+          onFocus={this.update}
+          onBlur={this.update}
+          onDoubleClick={this.update}
+          onTouchStart={this.update}
+          onTouchMove={this.update}
+          onTouchEnd={this.update}
+          cols="30"
+          rows="10"
+        />
+        <h1>{this.state.currentEvent}</h1>
+      </div>
     )
-  }
-}
-
-const Title = (props) => <h1>Title: {props.text}</h1>
-
-Title.propTypes = {
-  text(props, propName, component){
-    if(!(propName in props)){
-      return new Error(`missing ${propName}`)
-    }
-    if(props[propName].length < 6){
-      return new Error(`${propName} was too short`)
-    }
   }
 }
 
